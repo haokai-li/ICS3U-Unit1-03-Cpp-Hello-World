2bd56b42b908ccf580dfe3e1996c82dbae2e1994
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-12 21:33:32 +0000

    revise 13

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This is the "Hello world!" program, with proper style
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+}
